736d2d5d9f7899252b045c517741b9ddd3ec4f63
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2026-01-15 19:03:52 +0100

    config: add a simple dataclass based config system
    Config objects are also validated using Pydantic
    
    Breaking changes:
    - train test validation split is now configured together, easier to
      spot mistakes
    - toml is supported
    
    FIXME: only 2 of the old configs have been converted

---
+++ src/neuralpdr/config.py
@@ -0,0 +1,74 @@
+from dataclasses import dataclass
+from pathlib import Path
+import sys
+from typing import Literal, TypeAlias
+
+if sys.version_info >= (3, 11):
+    import tomllib
+else:
+    import tomli as tomllib
+
+from pydantic import Field, TypeAdapter
+import yaml
+
+LearningScheduler: TypeAlias = Literal["sgdr", "constant"]
+Activation: TypeAlias = Literal["tanh"]
+
+
+@dataclass
+class Split:
+    """The splits should normalise to unity."""
+
+    train: float
+    validate: float
+    test: float
+
+
+@dataclass
+class LearningScheme:
+    timeseries_fraction: float
+    epochs: int
+    lr_scheduler: LearningScheduler
+    warmup_epochs: int
+    learning_rate: float
+
+
+@dataclass
+class Conf:
+    start_index: int
+    end_index: int
+    batch_size: int
+    learning_rate: float
+    weight_scale: float
+    weight_decay: float
+    weight_truncation: float
+    enc_dec_depth: int
+    enc_dec_width: int
+    latent_depth: int
+    latent_width: int
+    latent_bottleneck: int
+    latent_final_activation: Activation
+    minimal_timeseries_length: int
+    train_test_val_split: Split
+    training_batch_subsampling: float
+    shuffle_every_n_epochs: int
+    aux_features: bool
+    save_file_path: Path
+    dataset_path: Path
+    input_features_file: Path  # FIXME: should this be in config, or data?
+    learning_schemes: list[LearningScheme] = Field(default_factory=list)
+    double_epochs_last_fraction: bool = False
+
+
+def read_conf(path: str | Path):
+    path = Path(path)
+    match path.suffix:
+        case ".yaml":
+            with open(path) as fh:
+                parsed = yaml.safe_load(fh)
+        case ".toml":
+            parsed = tomllib.loads(path.read_text())
+        case _:
+            raise RuntimeError(f"{path.suffix}: unsupported file format {path!r}")
+
+    return TypeAdapter(Conf).validate_python(parsed)
