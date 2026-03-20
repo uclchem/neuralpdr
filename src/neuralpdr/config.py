04ddbc6d601161116746a58c5f45e985e3c6b4ef
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2026-03-20 14:15:24 +0100

    config: extend config to support other config types

--- src/neuralpdr/config.py
+++ src/neuralpdr/config.py
@@ -1,7 +1,8 @@
 from dataclasses import asdict
+import json
 from pathlib import Path
 import sys
-from typing import Annotated, Literal, TypeAlias
+from typing import Annotated, Literal, TypeAlias, TypeVar
 
 if sys.version_info >= (3, 11):
     import tomllib
@@ -12,7 +13,7 @@ from pydantic import Field, TypeAdapter, model_validator
 from pydantic.dataclasses import dataclass
 
 LearningScheduler: TypeAlias = Literal["sgdr", "constant"]
-Activation: TypeAlias = Literal["tanh"]
+Activation: TypeAlias = Literal["tanh", "softplus"]
 ModelTypes: TypeAlias = Literal["latent", "fno"]
 
 
@@ -51,32 +52,45 @@ class _Base:
     weight_truncation: float
     enc_dec_depth: int
     enc_dec_width: int
-    minimal_timeseries_length: int
-    train_test_val_split: Split
-    training_batch_subsampling: float
-    shuffle_every_n_epochs: int
     aux_features: bool
     save_file_path: Path
     dataset_path: Path
     input_features_file: Path
-    learning_schemes: list[LearningScheme]
+    depth: int
+    width: int
 
 
 @dataclass(frozen=True)
-class Latent(_Base):
-    depth: int
-    width: int
+class _Train:
+    minimal_timeseries_length: int
+    train_test_val_split: Split
+    training_batch_subsampling: float
+    shuffle_every_n_epochs: int
+    learning_schemes: list[LearningScheme]
+    normalisations_file: Path
+    checkpoint_file: Path
+    checkpoint_epoch: int  # maybe needs a `None` default
+
+
+@dataclass(frozen=True)
+class _Latent:
     bottleneck: int
     final_activation: Activation
+
+
+@dataclass(frozen=True)
+class Latent(_Base, _Train, _Latent):
     double_epochs_last_fraction: bool = False
     model: Literal["latent"] = "latent"
 
 
 @dataclass(frozen=True)
-class FNO(_Base):
-    depth: int
-    width: int
+class _FNO:
     spectrum: list[float]
+
+
+@dataclass(frozen=True)
+class FNO(_Base, _Train, _FNO):
     double_epochs_last_fraction: bool = False
     model: Literal["fno"] = "fno"
 
@@ -88,12 +102,15 @@ def _read_toml(path: Path) -> dict:
     match path.suffix:
         case ".toml":
             return tomllib.loads(path.read_text())
+        case ".json":
+            return json.loads(path.read_text())
         case _:
             raise RuntimeError(f"{path.suffix}: unsupported file format {path!r}")
 
 
-def read_conf(path: str | Path):
-    path = Path(path)
+def read_conf(path: str | Path) -> Conf:
+    if not (path := Path(path)).exists():
+        raise RuntimeError(f"{path}: missing config file")
     parsed = _read_toml(path)
     return TypeAdapter(Conf).validate_python(parsed)
 
@@ -105,8 +122,46 @@ class Features:
     aux: list[str]
 
 
-def read_feature_list(path: Path):
-    if not path.exists():
-        raise RuntimeError(f"{path}: missing features file")
+@dataclass(frozen=True)
+class IVNorm:
+    eps: float = Field(default=0.0)
+    mean: float = Field(default=0.0)
+    std: float = Field(default=0.0)
+
+
+@dataclass(frozen=True)
+class AUXNorm:
+    eps: list[float] = Field(default_factory=lambda: [0.0])
+    mean: list[float] = Field(default_factory=lambda: [0.0])
+    std: list[float] = Field(default_factory=lambda: [0.0])
+
+
+@dataclass(frozen=True)
+class DataNorm:
+    eps: float = Field(default=0.0)
+    mean: list[float] = Field(default_factory=lambda: [0.0])
+    std: list[float] = Field(default_factory=lambda: [0.0])
+
+
+@dataclass(frozen=True)
+class Norms:
+    iv: IVNorm
+    aux: AUXNorm
+    data: DataNorm
+
+
+@dataclass(frozen=True)
+class DataMetadata:
+    train_indices: list[str]
+    val_indices: list[str]
+    test_indices: list[str]
+
+
+conf_t = TypeVar("conf_t", Features, Norms, DataMetadata)
+
+
+def read_as(path: str | Path, as_type: type[conf_t]) -> conf_t:
+    if not (path := Path(path)).exists():
+        raise RuntimeError(f"{path}: missing {as_type.__name__!r} file")
     parsed = _read_toml(path)
-    return TypeAdapter(Features).validate_python(parsed)
+    return TypeAdapter(as_type).validate_python(parsed)
